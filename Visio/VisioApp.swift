3b5abb6e890971f9d83ea39875a7829abbc1974f
Elias Alamat <ealamat@sandiego.edu>
2024-12-07 18:16:38 -0800

    Initial Commit

---
+++ Visio/VisioApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisioApp.swift
+//  Visio
+//
+//  Created by Elias Alamat on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct VisioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VisioTests/VisioTests.swift
@@ -0,0 +1,17 @@
+//
+//  VisioTests.swift
+//  VisioTests
+//
+//  Created by Elias Alamat on 12/7/24.
+//
+
+import Testing
+@testable import Visio
+
+struct VisioTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
